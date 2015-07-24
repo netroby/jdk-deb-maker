331a4a62ede436ef1465fe405e52c4b88cb8c5a9
ZhiFeng Hu <hzfeng@vip.deyi.com>
2015-07-24 08:25:41 +0800

    Update jdk-deb-make.sh

--- jdk-deb-make.sh
+++ jdk-deb-make.sh
@@ -11,7 +11,6 @@ PKG_BASENAME="jdk-$VERSION-linux-x64.tar.gz"
 URL="http://download.oracle.com/otn-pub/java/jdk/$VERSION-$BUILD/$PKG_BASENAME"
 
 PREFIX=/usr/lib/jvm/jdk-$VERSION
-DEBNAME_VIRTUAL="${PACKAGE_VIRTUAL}-${VERSION}_amd64.deb"
 DEBNAME="${PACKAGE}-${VERSION}_amd64.deb"
 
 mkdir -p work
@@ -28,23 +27,6 @@ then
 fi
 
 cd work
-
-if [[ ! -f "$DEBNAME_VIRTUAL" ]]
-then
-    echo "building $DEBNAME_VIRTUAL"
-    touch virtual
-    fpm -s dir -t deb -n $PACKAGE_VIRTUAL -v $VERSION -p $DEBNAME_VIRTUAL \
-        --prefix $PREFIX \
-        --depends $PACKAGE \
-        --provides java-jdk \
-        --provides java$VERSION_MAJOR-jdk \
-        --provides java-runtime \
-        --provides java$VERSION_MAJOR-runtime \
-        --provides default-jre \
-        --provides default-jre-headless \
-        --provides java$VERSION_MAJOR-runtime-headless \
-        --inputs virtual
-fi
 
 if [[ ! -f "$DEBNAME" ]]
 then
