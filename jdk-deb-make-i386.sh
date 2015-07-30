f2e8b4806eeb49ba5dfaa7552aeb4db9d244f00d
ZhiFeng Hu <hufeng1987@gmail.com>
2015-07-30 04:32:44 +0200

    Let i386 build on it's i386 dir

--- jdk-deb-make-i386.sh
+++ jdk-deb-make-i386.sh
@@ -6,6 +6,7 @@ VERSION_MAJOR=8
 VERSION_MINOR=51
 VERSION="${VERSION_MAJOR}u${VERSION_MINOR}"
 BUILD=b16
+ARCH=i386
 
 PKG_BASENAME="jdk-$VERSION-linux-i586.tar.gz"
 URL="http://download.oracle.com/otn-pub/java/jdk/$VERSION-$BUILD/$PKG_BASENAME"
@@ -13,20 +14,20 @@ URL="http://download.oracle.com/otn-pub/java/jdk/$VERSION-$BUILD/$PKG_BASENAME"
 PREFIX=/usr/lib/jvm/jdk-$VERSION
 DEBNAME="${PACKAGE}-${VERSION}_i386.deb"
 
-mkdir -p work
+mkdir -p work/$ARCH
 
-if [ ! -f "work/$PKG_BASENAME" ]
+if [ ! -f "work/$ARCH/$PKG_BASENAME" ]
 then
     echo "downloading from $URL"
     # bypass manual LICENSE confirmation: http://bit.ly/1hUJBDC
     # see also PKGBUILD from AUR: http://bit.ly/1n7VhYr
-    wget -c -O work/$PKG_BASENAME \
+    wget -c -O work/$ARCH/$PKG_BASENAME \
          --no-cookies \
          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
          $URL
 fi
 
-cd work
+cd work/$ARCH
 
 if [ ! -f "$DEBNAME" ]
 then
@@ -37,7 +38,7 @@ then
         --conflicts openjdk-$VERSION_MAJOR-jdk \
         --conflicts openjdk-$VERSION_MAJOR-jre \
         --template-scripts \
-        --after-install ../post-install.sh \
+        --after-install ../../post-install.sh \
         -C "jdk1.${VERSION_MAJOR}.0_${VERSION_MINOR}" \
         .
 fi
